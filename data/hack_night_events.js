1f1353deea292e28f36c1acb2313960882e7db33
Derek Eder <derek.eder@gmail.com>
2013-09-11 11:04:52 -0500

    started on events.json page, listing two events (relates to #4)

---
+++ data/hack_night_events.js
@@ -0,0 +1,16 @@
+var hack_night_events = [
+  {
+    "id": "1",
+    "title": "Smart Cities and Simulation",
+    "date": "2013-09-10",
+    "description": "Matthew Shaxted will share recent projects that investigate smart cities using data analysis, webGL visualization and agent-based simulation.",
+    "sponsor": "Code for America"
+  },
+  {
+    "id": "2",
+    "title": "Go To School!",
+    "date": "2013-09-03",
+    "description": "<p>Tom Kompare will discuss his experience building <a href='http://cps.go2school.org/'>Go To School</a>, an app developed built with volunteer labor in partnership with <a href='http://cps.edu/Pages/home.aspx'>Chicago Public Schools</a> to help parents get their kids to school safely and on time.</p><p>We will also discuss the <a href='https://data.cityofchicago.org/browse?q=cps&sortBy=relevance&utf8=%E2%9C%93'>data that CPS makes available</a>, and how it can be used to make other useful apps for Chicagoans.</p>",
+    "sponsor": "DataMade"
+  }
+]
