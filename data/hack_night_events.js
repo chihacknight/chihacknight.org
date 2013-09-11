ee3c1ed58dfa55066fcd4386e6049d62c1523e41
Derek Eder <derek.eder@gmail.com>
2013-09-11 15:30:27 -0500

    spiffed up the date display, implemented custom datatables sorting for dates, added more events(related to #4)

--- data/hack_night_events.js
+++ data/hack_night_events.js
@@ -4,13 +4,41 @@ var hack_night_events = [
     "title": "Smart Cities and Simulation",
     "date": "2013-09-10",
     "description": "Matthew Shaxted will share recent projects that investigate smart cities using data analysis, webGL visualization and agent-based simulation.",
-    "sponsor": "Code for America"
+    "sponsor": "<a href='http://codeforamerica.org'>Code for America</a>"
   },
   {
     "id": "2",
     "title": "Go To School!",
     "date": "2013-09-03",
     "description": "<p>Tom Kompare will discuss his experience building <a href='http://cps.go2school.org/'>Go To School</a>, an app developed built with volunteer labor in partnership with <a href='http://cps.edu/Pages/home.aspx'>Chicago Public Schools</a> to help parents get their kids to school safely and on time.</p><p>We will also discuss the <a href='https://data.cityofchicago.org/browse?q=cps&sortBy=relevance&utf8=%E2%9C%93'>data that CPS makes available</a>, and how it can be used to make other useful apps for Chicagoans.</p>",
-    "sponsor": "DataMade"
-  }
+    "sponsor": "<a href='http://datamade.us'>DataMade</a>"
+  },
+  {
+    "id": "3",
+    "title": "Data Science for Social Good Recap",
+    "date": "2013-08-27",
+    "description": "<p>Tom Plagge and Juan-Pablo Velez will give a brief retrospective of the <a href=''>Data Science for Social Good</a> summer fellowship program at UChicago where students with CS and statistics backgrounds work on real-world projects with governments and nonprofits.</p><p>The fellowship wrapped up last Friday, and Tom and Juan will share results from projects involving <a href='http://dssg.io/2013/08/02/why-bus-crowding-happens.html'>the CTA</a>, Chicago Streets and Sanitation, the <a href='http://dssg.io/2013/07/11/cook-county-land-bank.html'>Cook County Land Bank</a>, and Northshore Hospital.</p>",
+    "sponsor": "<a href='http://thoughtworks.com'>Thoughtworks</a>"
+  },
+  {
+    "id": "4",
+    "title": "Englewood Codes Recap",
+    "date": "2013-08-20",
+    "description": "<p>Demond Drummer of <a href='http://www.teamworkenglewood.org/'>Teamwork Englewood</a> will talk about lessons learned and what's next for <a href='http://www.teamworkenglewood.org/Smart-Communities-Program/Englewood-Codes.html?utm_source=%2fcode&utm_medium=web&utm_campaign=redirect'>Englewood Codes</a>, a <a href='http://www.kickstarter.com/projects/1348602678/englewood-codes'>Kickstarter</a> and <a href='http://www.afterschoolmatters.org/'>After School Matters</a> funded project where 25 teens from the Englewood community spent the summer learning HTML, CSS and JavaScript and hacking around with the <a href='http://www.raspberrypi.org/'>Raspberry Pi</a>.</p>",
+    "sponsor": "<a href='http://openshift.com/'>OpenShift</a>"
+  },
+    {
+      "id": "5",
+      "title": "Department of Labor App Challenge",
+      "date": "2013-08-13",
+      "description": "<p>Matt Bruce with the <a href=''>Chicago Community Trust</a> and <a href='http://rocunited.org/'>Restaurant Opportunities Centers United</a> will talk about the US Labor Department's <a href='http://fairlabor.challengepost.com/'>app challenge</a> for creating a smartphone app that integrates the department’s publicly available enforcement data with consumer ratings, geo-positioning, and other relevant data sets. <a href='http://www.smartchicagocollaborative.org/chicago-developers-us-department-of-labor-launches-smartphone-app-contest-around-fair-labor/'>More details here</a>.</p>",
+      "sponsor": "<a href='http://smartchicagocollaborative.org'>Smart Chicago Collaborative</a>"
+    },
+    {
+      "id": "6",
+      "title": "DataMandering",
+      "date": "2013-08-6",
+      "description": "<p>John Fahrenbach will talk about his <a href='http://datamandering.org/'>Datamandering</a> project, an online tool for demystifying the <a href='http://en.wikipedia.org/wiki/Gerrymandering'>gerrymandering</a> process with open data.</p>",
+      "sponsor": "<a href='http://thoughtworks.com'>Thoughtworks</a>"
+    }
 ]
