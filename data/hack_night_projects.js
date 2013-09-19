8ba2907186d2f4c2739f2b8894d0236cffd65a09
Derek Eder <derek.eder@gmail.com>
2013-09-19 11:00:08 -0500

    first pass at marking up projects page with github info

--- data/hack_night_projects.js
+++ data/hack_night_projects.js
@@ -1,4 +1,54 @@
 var hack_night_projects = [
-    "github.com/open-city/civic-hack-night",
-    "github.com/NYCComptroller/Checkbook",
+  {
+    "id": 11456727,
+    "name": "census-communities-usa",
+    "description": "Mapping and analyzing local business data from the Census Bureau",
+    "homepage": null,
+    "html_url": "https://github.com/dssg/census-communities-usa",
+    "language": "Python",
+    "watchers_count": 8,
+    "forks_count": 4,
+    "open_issues": 7,
+    "owner": {
+      "login": "dssg",
+      "html_url": "https://github.com/dssg",
+      "avatar_url": "https://0.gravatar.com/avatar/1aea09ad5b51a39e35a2108e11e5a403?d=https%3A%2F%2Fidenticons.github.com%2Fdb130dc016d1828fac0644f7f0d7d6bf.png"
+    },
+    "contributors_url": "https://api.github.com/repos/cnunciato/jekyll-starter/contributors",
+    "contributors": {
+      {
+        "login": "hunterowens",
+        "avatar_url": "https://2.gravatar.com/avatar/ff16180e6d9715d5ae17526cb83d7cfb?d=https%3A%2F%2Fidenticons.github.com%2Fc3663e10b721e5d713a55aebf8621901.png",
+        "html_url": "https://github.com/hunterowens",
+        "contributions": 74
+      },
+      {
+        "login": "evz",
+        "avatar_url": "https://1.gravatar.com/avatar/1d0c5faee140af87d7d6967bc946ecc6?d=https%3A%2F%2Fidenticons.github.com%2F44e80db9ed8527f429c969e804432b0f.png",
+        "html_url": "https://github.com/evz",
+        "contributions": 55
+      },
+      {
+        "login": "fishmanadam",
+        "avatar_url": "https://2.gravatar.com/avatar/d1529c1f159f49be15cf5fde0366b599?d=https%3A%2F%2Fidenticons.github.com%2Fdb534c76db466f7764d7e745900be88d.png",
+        "html_url": "https://github.com/fishmanadam",
+        "contributions": 7
+      },
+      {
+        "login": "jtwalsh0",
+        "avatar_url": "https://0.gravatar.com/avatar/05e1d117da7a62657f617bd553f4ed83?d=https%3A%2F%2Fidenticons.github.com%2F31d750398f38453e376cf435deb5fea9.png",
+        "html_url": "https://github.com/jtwalsh0",
+        "contributions": 2
+      },
+      {
+        "login": "matthewgee",
+        "avatar_url": "https://1.gravatar.com/avatar/1dbb7a239d59f1effc696f6424050a5a?d=https%3A%2F%2Fidenticons.github.com%2F9bf88927e24ab526ed285da976feecf0.png",
+        "html_url": "https://github.com/matthewgee",
+        "contributions": 1
+      }
+    },
+    "created_at": "2013-07-16T18:19:47Z",
+    "updated_at": "2013-09-18T01:16:31Z"
+    }
+  }
 ]
