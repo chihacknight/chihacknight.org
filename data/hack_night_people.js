7eb89b0b1b0014a3a9a44212ae2e5130d4db356b
Derek Eder <derek.eder@gmail.com>
2013-09-22 09:29:11 -0500

    stubbed out people page #10

---
+++ data/hack_night_people.js
@@ -0,0 +1,37 @@
+var hack_night_people = [
+  {
+    "login": "hunterowens",
+    "avatar_url": "https://2.gravatar.com/avatar/ff16180e6d9715d5ae17526cb83d7cfb?d=https%3A%2F%2Fidenticons.github.com%2Fc3663e10b721e5d713a55aebf8621901.png",
+    "html_url": "https://github.com/hunterowens",
+    "contributions": 74,
+    "repositories": 5
+  },
+  {
+    "login": "evz",
+    "avatar_url": "https://1.gravatar.com/avatar/1d0c5faee140af87d7d6967bc946ecc6?d=https%3A%2F%2Fidenticons.github.com%2F44e80db9ed8527f429c969e804432b0f.png",
+    "html_url": "https://github.com/evz",
+    "contributions": 55,
+    "repositories": 3
+  },
+  {
+    "login": "fishmanadam",
+    "avatar_url": "https://2.gravatar.com/avatar/d1529c1f159f49be15cf5fde0366b599?d=https%3A%2F%2Fidenticons.github.com%2Fdb534c76db466f7764d7e745900be88d.png",
+    "html_url": "https://github.com/fishmanadam",
+    "contributions": 7,
+    "repositories": 2
+  },
+  {
+    "login": "jtwalsh0",
+    "avatar_url": "https://0.gravatar.com/avatar/05e1d117da7a62657f617bd553f4ed83?d=https%3A%2F%2Fidenticons.github.com%2F31d750398f38453e376cf435deb5fea9.png",
+    "html_url": "https://github.com/jtwalsh0",
+    "contributions": 2,
+    "repositories": 2
+  },
+  {
+    "login": "matthewgee",
+    "avatar_url": "https://1.gravatar.com/avatar/1dbb7a239d59f1effc696f6424050a5a?d=https%3A%2F%2Fidenticons.github.com%2F9bf88927e24ab526ed285da976feecf0.png",
+    "html_url": "https://github.com/matthewgee",
+    "contributions": 1,
+    "repositories": 1
+  }
+]
