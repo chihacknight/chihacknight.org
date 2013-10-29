0e4d31c1f0f159e8385bce4489c86e43a7f862a9
Derek Eder <derek.eder@gmail.com>
2013-10-29 17:58:12 -0500

    added 2 CNT events in 10/2013

--- data/hack_night_events.js
+++ data/hack_night_events.js
@@ -21,8 +21,20 @@ var hack_night_events = [
     ]
   },
   {
-    "title": "Center for Neighborhood Technology Hackathon",
+    "title": "Center for Neighborhood Technology Hackathon III",
+    "date": "2013-10-15",
+    "description": "<p>Part III of the Center for Neighborhood Technology Hackathon</p>",
+    "sponsor": "<a href='http://www.cnt.org/'>Center for Neighborhood Technology</a>"
+  },
+  {
+    "title": "Center for Neighborhood Technology Hackathon II",
     "date": "2013-10-08",
+    "description": "<p>Part II of the Center for Neighborhood Technology Hackathon</p>",
+    "sponsor": "<a href='http://www.cnt.org/'>Center for Neighborhood Technology</a>"
+  },
+  {
+    "title": "Center for Neighborhood Technology Hackathon I",
+    "date": "2013-10-01",
     "description": "<p>We're having a joint event with the <a href='http://www.cnt.org/'>Center for Neighborhood Technology</a>. CNT is doing their annual <a href='http://www.cnt.org/events/reinventing-chicago-2013-urban-sustainability-apps-competition/'>Urban Sustainability Apps Competition</a>, where community activists and app developers will work together to build tools to that make Chicago neighborhoods more sustainable, both environmentally and economically.</p><p>The main event is a weekend hackathon from Oct 18th-20th, but teams will form and start working at the hack night this week and next. (Of course, you don't have to participate in CNT's hackathon to show up!)</p>",
     "sponsor": "<a href='http://www.cnt.org/'>Center for Neighborhood Technology</a>"
   },
