7e3cd0061ec4868ab20dcdf2139e6762278ef980
Derek Eder <derek.eder@gmail.com>
2013-10-28 23:41:34 -0500

    added a few more events #4

--- data/hack_night_events.js
+++ data/hack_night_events.js
@@ -1,6 +1,5 @@
 var hack_night_events = [
   {
-    "id": "104",
     "title": "The City of Chicago Data Dictionary",
     "date": "2013-10-29",
     "description": "<p><a href='https://twitter.com/tomschenkjr'>Tom Schenk</a> will discuss the recently launched <a href='http://datadictionary.cityofchicago.org/'>Chicago Data Dictionary</a>, a website that serves as a single, comprehensive database catalog for the City of Chicago and City of Chicago sister agencies. The City has released the underlying code for this dictionary as open source, titled <a href='https://github.com/chicago/metalicious'>Metalicious</a>.</p>",
@@ -12,7 +11,6 @@ var hack_night_events = [
     ]
   },
   {
-    "id": "103",
     "title": "The Bike Index",
     "date": "2013-10-22",
     "description": "<p>Seth Herr will be talking about <a href='https://www.bikeindex.org/'>the Bike Index</a>, an <a href='https://github.com/bikeindex/webapp'>open source</a> bike registry to fight bike theft. Herr & co just had super successful <a href='http://www.kickstarter.com/projects/1073266317/the-bike-index-lets-stop-bike-theft-together'>Kickstarter campaign</a>.</p>",
@@ -23,14 +21,12 @@ var hack_night_events = [
     ]
   },
   {
-    "id": "102",
     "title": "Center for Neighborhood Technology Hackathon",
     "date": "2013-10-08",
     "description": "<p>We're having a joint event with the <a href='http://www.cnt.org/'>Center for Neighborhood Technology</a>. CNT is doing their annual <a href='http://www.cnt.org/events/reinventing-chicago-2013-urban-sustainability-apps-competition/'>Urban Sustainability Apps Competition</a>, where community activists and app developers will work together to build tools to that make Chicago neighborhoods more sustainable, both environmentally and economically.</p><p>The main event is a weekend hackathon from Oct 18th-20th, but teams will form and start working at the hack night this week and next. (Of course, you don't have to participate in CNT's hackathon to show up!)</p>",
     "sponsor": "<a href='http://www.cnt.org/'>Center for Neighborhood Technology</a>"
   },
   {
-    "id": "101",
     "title": "Chicago Works for You",
     "date": "2013-09-24",
     "description": "<p><a href='https://twitter.com/cgansen'>Chris Gansen</a> will demo <a href='http://chicagoworksforyou.com/'>Chicago Works for You</a>, a citywide dashboard by <a href='http://www.smartchicagocollaborative.org/'>Smart Chicago</a> that displays ward-by-ward views of city services in Chicago. <a href='http://www.smartchicagocollaborative.org/the-launch-of-chicago-works-for-you/'>Click here</a> for more info on the app, and <a href='http://www.smartchicagocollaborative.org/building-chicago-works-for-you/'>here</a> for how it was made. The app's github repo is <a href='https://github.com/smartchicago/chicagoworksforyou/'>here</a>.</p>",
@@ -42,14 +38,12 @@ var hack_night_events = [
     ]
   },
   {
-    "id": "100",
     "title": "Open Hack Session",
     "date": "2013-09-17",
     "description": "<p>No presenters - just come ready to build, chat and learn with other civic hackers, government do-gooders, and curious citizens!</p>",
     "sponsor": "<a href='https://www.openshift.com'>Open Shift</a>"
   },
   {
-    "id": "99",
     "title": "Smart Cities and Simulation",
     "date": "2013-09-10",
     "description": "<p>Matthew Shaxted will share recent projects that investigate smart cities using data analysis, webGL visualization and agent-based simulation.</p>",
@@ -59,7 +53,6 @@ var hack_night_events = [
     ]
   },
   {
-    "id": "98",
     "title": "Go To School!",
     "date": "2013-09-03",
     "description": "<p><a href='https://twitter.com/tomkompare'>Tom Kompare</a> will discuss his experience building <a href='http://cps.go2school.org/'>Go To School</a>, an app developed built with volunteer labor in partnership with <a href='http://cps.edu/Pages/home.aspx'>Chicago Public Schools</a> to help parents get their kids to school safely and on time.</p><p>We will also discuss the <a href='https://data.cityofchicago.org/browse?q=cps&sortBy=relevance&utf8=%E2%9C%93'>data that CPS makes available</a>, and how it can be used to make other useful apps for Chicagoans.</p>",
@@ -71,7 +64,6 @@ var hack_night_events = [
     ]
   },
   {
-    "id": "97",
     "title": "Data Science for Social Good Recap",
     "date": "2013-08-27",
     "description": "<p><a href='https://twitter.com/tplagge'>Tom Plagge</a> and <a href='https://twitter.com/jpvelez'>Juan-Pablo Velez</a> will give a brief retrospective of the <a href=''>Data Science for Social Good</a> summer fellowship program at UChicago where students with CS and statistics backgrounds work on real-world projects with governments and nonprofits.</p><p>The fellowship wrapped up last Friday, and Tom and Juan will share results from projects involving <a href='http://dssg.io/2013/08/02/why-bus-crowding-happens.html'>the CTA</a>, Chicago Streets and Sanitation, the <a href='http://dssg.io/2013/07/11/cook-county-land-bank.html'>Cook County Land Bank</a>, and Northshore Hospital.</p>",
@@ -82,7 +74,6 @@ var hack_night_events = [
     ]
   },
   {
-    "id": "96",
     "title": "Englewood Codes Recap",
     "date": "2013-08-20",
     "description": "<p><a href='https://twitter.com/citizendrummer'>Demond Drummer</a> of <a href='http://www.teamworkenglewood.org/'>Teamwork Englewood</a> will talk about lessons learned and what's next for <a href='http://www.teamworkenglewood.org/Smart-Communities-Program/Englewood-Codes.html?utm_source=%2fcode&utm_medium=web&utm_campaign=redirect'>Englewood Codes</a>, a <a href='http://www.kickstarter.com/projects/1348602678/englewood-codes'>Kickstarter</a> and <a href='http://www.afterschoolmatters.org/'>After School Matters</a> funded project where 25 teens from the Englewood community spent the summer learning HTML, CSS and JavaScript and hacking around with the <a href='http://www.raspberrypi.org/'>Raspberry Pi</a>.</p>",
@@ -93,14 +84,12 @@ var hack_night_events = [
     ]
   },
   {
-    "id": "95",
     "title": "Department of Labor App Challenge",
     "date": "2013-08-13",
     "description": "<p>Matt Bruce with the <a href=''>Chicago Community Trust</a> and <a href='http://rocunited.org/'>Restaurant Opportunities Centers United</a> will talk about the US Labor Department's <a href='http://fairlabor.challengepost.com/'>app challenge</a> for creating a smartphone app that integrates the department’s publicly available enforcement data with consumer ratings, geo-positioning, and other relevant data sets. <a href='http://www.smartchicagocollaborative.org/chicago-developers-us-department-of-labor-launches-smartphone-app-contest-around-fair-labor/'>More details here</a>.</p>",
     "sponsor": "<a href='http://smartchicagocollaborative.org'>Smart Chicago Collaborative</a>"
   },
   {
-    "id": "94",
     "title": "DataMandering",
     "date": "2013-08-06",
     "description": "<p>John Fahrenbach will talk about his <a href='http://datamandering.org/'>Datamandering</a> project, an online tool for demystifying the <a href='http://en.wikipedia.org/wiki/Gerrymandering'>gerrymandering</a> process with open data.</p>",
@@ -111,7 +100,6 @@ var hack_night_events = [
     ]
   },
   {
-    "id": "93",
     "title": "Food Inspection App by Hoy",
     "date": "2013-07-30",
     "description": "<p><a href='http://www.linkedin.com/in/fdiazreporting'>Fernando Diaz</a> and <a href='https://github.com/wilbertom'>Wilberto Morales</a> from Hoy will demo their new food inspection app: <a href='http://www.eatsafe.co'>Eat Safe</a>. Come ready to talk about food safety data!</p>",
@@ -122,7 +110,6 @@ var hack_night_events = [
     ]
   },
   {
-    "id": "92",
     "title": "Mining 311 Calls",
     "date": "2013-07-23",
     "description": "<p>One of the <a href='http://dssg.io'>Data Science for Social Good</a> teams will talk their work <a href='http://dssg.io/projects/#city'>data mining 311 calls</a> and what they're learning about Chicago's neighborhoods.</p>",
@@ -132,7 +119,6 @@ var hack_night_events = [
     ]
   },
   {
-    "id": "91",
     "title": "Divvy Trip Planner",
     "date": "2013-07-16",
     "description": "<p>Following up from the Hack Night on July 2nd, Adam and Andrew from <a href='http://bglconsulting.com/'>BGL Consulting</a> will talk about their <a href='http://www.stevencanplan.com/2013/new-ios-app-offers-most-advanced-divvy-route-directions/'>Divvy trip planner for iOS</a>, as well as an <a href='https://github.com/AdamGluck/DivvyAPI'>open source DivvyAPI wrapper</a> for creating iPhone apps with Divvy data.</p><p>We'll also get a follow-up on the <a href='http://hackchitown-eorg.eventbrite.com/'>AT&T Hackathon</a> from this weekend and discuss the projects that came out of it.</p>",
@@ -144,14 +130,12 @@ var hack_night_events = [
     ]
   },
   {
-    "id": "90",
     "title": "Open Hack Session",
     "date": "2013-07-09",
     "description": "<p>No presenters - just come ready to build, chat and learn with other civic hackers, government do-gooders, and curious citizens!</p>",
     "sponsor": "<a href='https://thoughtworks.com'>Thoughtworks</a>"
   },
   {
-    "id": "89",
     "title": "Divvy Data!",
     "date": "2013-07-02",
     "description": "<p><a href='https://twitter.com/stevevance'>Steven Vance</a> of <a href='http://chi.streetsblog.org/'>Streetsblog Chicago</a> will give an overview of <a href='http://divvybikes.com/'>Divvy</a>, Chicago's new bike share program. We'll look in to the open data that is <a href='http://divvybikes.com/stations/json'>currently available</a>, and the apps that are possible with it. Let's hack some bike share data!</p>",
@@ -164,10 +148,40 @@ var hack_night_events = [
     ]
   },
   {
-    "id": "90",
+    "title": "Data Potluck #9: Data Science for Social Good",
+    "date": "2013-06-25",
+    "description": "<p>The University of Chicago is doing a Data Science for Social Good fellowship this summer. They've brought 36 aspiring data scientist to Chicago to work on data science projects with city government, federal agencies, and nonprofits from throughout the country. The fellowship is funded by Google chairman Eric Schmidt and is funded by former chief data scientist for the Obama 2012 campaign Rayid Ghani.</p>",
+    "sponsor": "<a href='http://dssg.io'>Data Science for Social Good</a>",
+    "links": [
+      {"name": "Data Science for Social Good", "url": "http://dssg.io"},
+      {"name": "Data Potluck #9", "url": "http://www.meetup.com/DataPotluck/events/125444002/"},
+      {"name": "Livestream on YouTube", "url": "http://www.youtube.com/watch?v=5C0haCUjeQM"},
+    ]
+  },
+  {
+    "title": "Checkbook NYC, Learning Fusion Tables",
+    "date": "2013-06-18",
+    "description": "<p>New York City recently released <a href='http://www.checkbooknyc.com/'>Checkbook NYC</a>, a transparency app that shows the city's budgets, contracts, and payrolls. They also took the unprecedented step of <a href='https://github.com/NYCComptroller/Checkbook'>open sourcing the code</a>, and our our friend <a href='http://www.red-bean.com/kfogel/'>Karl Fogel</a> helped them do it. Karl will talk about the project, the backstory of how it was opened, and his plans to stand it up in Chicago.</p><p>Following on last week's git tutorial, <a href='http://derekeder.com/'>Derek Eder</a> will be teaching interested people the basics of making maps with <a href='https://support.google.com/fusiontables/answer/2571232?hl=en'>Google Fusion Tables</a>. Always wanted to learn? Now's your chance.</p>",
+    "sponsor": "<a href='http://codeforamerica.org'>Code for America</a>",
+    "links": [
+      {"name": "Checkbook NYC", "url": "http://www.checkbooknyc.com/"},
+      {"name": "Checkbook NYC Github repo", "url": "https://github.com/NYCComptroller/Checkbook"},
+      {"name": "Searchable Map Template", "url": "http://derekeder.com/searchable_map_template/"},
+    ]
+  },
+  {
+    "title": "Intro to Git and Github",
+    "date": "2013-06-11",
+    "description": "<p>In addition to working on civic tech projects, Derek is going to be leading an intro to Git and Github tutorial for folks who want to learn.</p>",
+    "sponsor": "<a href='http://thoughtworks.com'>Thoughtworks</a>",
+    "links": [
+      {"name": "Github 101 - YouTube", "url": "http://www.youtube.com/watch?v=ZxabbwjcuIE"},
+    ]
+  },
+  {
     "title": "The First Hack Night",
     "date": "2012-03-22",
     "description": "<p>The first Open Gov Hack Night was hosted at <a href='http://www.webitects.com/'>Webitects</a> and attended by <a href='https://twitter.com/derekeder'>Derek Eder</a>, <a href='https://twitter.com/jpvelez'>Juan-Pablo Velez</a>, <a href='https://twitter.com/srobbin'>Scott Robbin</a> and <a href='https://twitter.com/tomkompare'>Tom Kompare</a>.</p>",
     "sponsor": "None"
-  },
+  }
 ]
