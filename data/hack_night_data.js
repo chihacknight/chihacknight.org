c1bcda03adfa38fbc9300eba8bdd08f385ffe7de
Derek Eder <derek.eder@gmail.com>
2013-11-13 12:19:35 -0600

    added data resources #28

--- data/hack_night_data.js
+++ data/hack_night_data.js
@@ -1,6 +1,7 @@
 var hack_night_data = [
   {
     "title": "Chicago Data Portal",
+    "category": "Municipal",
     "url": "http://data.cityofchicago.org",
     "description": "Official data portal for the City of Chicago. Contains hundreds of datasets including  crime, health, energy, budgets, salaries and more!",
     "links": [
@@ -10,6 +11,7 @@ var hack_night_data = [
   },
   {
     "title": "Chicago Transit Authority API",
+    "category": "Transit",
     "url": "http://www.transitchicago.com/developers/default.aspx",
     "description": "API for the Chicago Transit Authority (CTA) buses and trains.",
     "links": [
@@ -19,11 +21,13 @@ var hack_night_data = [
   },
   {
     "title": "Divvy API",
+    "category": "Transit",
     "url": "http://divvybikes.com/stations/json",
     "description": "List of stations and available bikes for Chicago's bike share system."
   },
   {
     "title": "Open 311 API",
+    "category": "Municipal",
     "url": "http://dev.cityofchicago.org/docs/api",
     "description": "Open311 is a form of technology that provides open channels of communication for issues that concern public space and public services. Primarily, Open311 refers to a standardized protocol for location-based collaborative issue-tracking.",
     "links": [
@@ -33,14 +37,16 @@ var hack_night_data = [
   },
   {
     "title": "Chicago Police ClearPath API",
+    "category": "Crime",
     "url": "http://api1.chicagopolice.org/clearpath/documentation",
-    "description": "Chicago Police Department data on community concerns, crime, mugshots, most wanted and events.",
+    "description": "Chicago Police Department (CPD) data on community concerns, crime, mugshots, most wanted and events.",
     "links": [
       {"name": "News: Smart Chicago blog post on ClearPath API", "url": "http://www.smartchicagocollaborative.org/using-the-new-clearpath-api-to-help-communities-interact-with-the-chicago-police-department/"}
     ]
   },
   {
     "title": "OpenStreetMap",
+    "category": "Geography",
     "url": "http://openstreetmap.org",
     "description": "A wiki map, built on data submitted by volunteers and public data sources. The data is available and free to use through the OpenStreetMap API and bulk downloads.",
     "links": [
@@ -51,6 +57,7 @@ var hack_night_data = [
   },
   {
     "title": "Cook County Data Portal",
+    "category": "Municipal",
     "url": "http://data.cookcountyil.gov/",
     "description": "Open data portal for Cook County government.",
     "links": [
@@ -59,24 +66,83 @@ var hack_night_data = [
   },
   {
     "title": "State of Illinois Data Portal",
+    "category": "Municipal",
     "url": "https://data.illinois.gov/",
     "description": "Open data portal for the State of Illinois.",
   },
   {
     "title": "census.ire.org",
+    "category": "Demographics",
     "url": "http://census.ire.org/",
-    "description": "Easiest way to get demographic data from the Census Bureau - way easier than census.gov.",
+    "description": "Site designed to provide journalists with a simpler way to access 2000 and 2010 Census data so they can spend less time importing and managing the data and more time exploring and reporting the data.",
     "links": [
       {"name": "News: Knight funding expands IRE’s journalist-friendly Census site", "url": "http://www.niemanlab.org/2012/10/knight-funding-expands-ires-journalist-friendly-census-site/"}
     ]
   },
   {
     "title": "Institute for Housing Studies Data Portal",
+    "category": "Housing",
     "url": "http://www.housingstudies.org/dataportal/info/",
     "description": "Data on housing trends for Chicago-area communities.",
     "links": [
       {"name": "Tutorial: Exploring Property Sales Data", "url": "http://www.housingstudies.org/news/blog/exploring-property-sales-data-ihs-data-portal/"},
-      {"name": "Tutorial: Using Mortgage Data", "url": "http://www.housingstudies.org/news/blog/using-mortgage-data-ihs-data-portal/"},
+      {"name": "Tutorial: Using Mortgage Data", "url": "http://www.housingstudies.org/news/blog/using-mortgage-data-ihs-data-portal/"}
+    ]
+  },
+  {
+    "title": "Woodstock Institute Data Portal",
+    "category": "Housing",
+    "url": "http://www.woodstockinst.org/content/foreclosure",
+    "description": "Data for the Chicago six county region on foreclosures, mortgage applications, housing, income and employment.",
+    "links": [
+      {"name": "Tutorial: Woodstock Institute Data Portal Training", "url": "http://www.woodstockinst.org/research/woodstock-institute-data-portal-training"}
+    ]
+  },
+  {
+    "title": "Chicago Tribune Boundary Service",
+    "category": "Municipal geography",
+    "url": "http://boundaries.tribapps.com/api/",
+    "description": "The Boundary Service API provides a restful interface to a wealth of data about official geographic boundaries within Illinois. Data is retrieved from the API as JSON (optionally JSONP) with nested GeoJSON for describing the boundary shapes.",
+    "links": [
+      {"name": "Demo: You are here", "url": "http://boundaries.tribapps.com/"},
+      {"name": "Steal this code: The Newsapps Boundary Service API!", "url": "http://blog.apps.chicagotribune.com/2011/01/28/steal-this-code-the-newsapps-boundary-service-api/"}
+    ]
+  },
+  {
+    "title": "Chicago Tribune Crime API",
+    "category": "Crime",
+    "url": "https://github.com/newsapps/chicagocrime/blob/master/docs/api_docs.md",
+    "description": "The Boundary Service API provides a restful interface to a wealth of data about official geographic boundaries within Illinois. Data is retrieved from the API as JSON (optionally JSONP) with nested GeoJSON for describing the boundary shapes.",
+    "links": [
+      {"name": "Crime in Chicago", "url": "http://crime.chicagotribune.com/"},
+      {"name": "News: Announcing the Chicago Crime API", "url": "http://blog.apps.chicagotribune.com/2013/03/22/announcing-the-chicago-crime-api/"}
+    ]
+  },
+  {
+    "title": "CTA 'Unofficial' API",
+    "category": "Transit",
+    "url": "http://harperreed.github.io/transitapi/",
+    "description": "In 2009, the Chicago's Transit Authority released a couple webapps that allowed people to track bus locations and routes. Harper Reed created this unofficial API documentation based on how those webapps worked.",
+    "links": [
+      {"name": "Github repo", "url": "https://github.com/harperreed/transitapi/"}
+    ]
+  },
+  {
+    "title": "Illinois school report cards",
+    "category": "Schools",
+    "url": "http://schools.chicagotribune.com/data-api.html",
+    "description": "Each year, the Illinois State Board of Education releases 'report cards,'' detailed analyses of the performance of public schools and school districts in Illinois, in accordance with state and federal law. The Chicago Tribune has made this data available via an API.",
+    "links": [
+      {"name": "2013 Illinois school report cards", "url": "http://schools.chicagotribune.com/"}
+    ]
+  },
+  {
+    "title": "Chicago City Council legislation (CSV file)",
+    "category": "Legislation",
+    "url": "https://s3.amazonaws.com/councilmatic-chicago/councilmatic-data.zip",
+    "description": "Every law, ordinance and resolution passed or proposed by Chicago City Council since Jan 1st, 2010. Used to power Chicago Councilmatic by Open City.",
+    "links": [
+      {"name": "Chicago Councilmatic", "url": "http://chicagocouncilmatic.org/"}
     ]
   }
 ]
