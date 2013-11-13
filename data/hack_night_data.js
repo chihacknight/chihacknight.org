a30676e100f8c3cc21c21d188d25822b0a7a2914
Derek Eder <derek.eder@gmail.com>
2013-11-12 20:22:59 -0600

    added more data resources, styles

--- data/hack_night_data.js
+++ data/hack_night_data.js
@@ -5,16 +5,46 @@ var hack_night_data = [
     "description": "Official data portal for the City of Chicago. Contains hundreds of datasets including  crime, health, energy, budgets, salaries and more!",
     "links": [
       {"name": "Soda 2.0 API", "url": "http://dev.socrata.com/consumers/getting-started"},
-      {"name": "Energy Usage 2010 API", "url": "https://data.cityofchicago.org/developers/docs/energy-usage-2010"},
+      {"name": "Energy Usage 2010 API", "url": "https://data.cityofchicago.org/developers/docs/energy-usage-2010"}
     ]
   },
   {
-    "title": "Chicago Transit Authority API",
+    "title": "CTA API",
     "url": "http://www.transitchicago.com/developers/default.aspx",
-    "description": "API for the CTA buses and trains.",
+    "description": "API for the Chicago Transit Authority (CTA) buses and trains.",
     "links": [
       {"name": "CTA Train Tracker API", "url": "http://www.transitchicago.com/developers/traintracker.aspx"},
-      {"name": "Bus Tracker API", "url": "http://www.transitchicago.com/developers/bustracker.aspx"},
+      {"name": "Bus Tracker API", "url": "http://www.transitchicago.com/developers/bustracker.aspx"}
+    ]
+  },
+  {
+    "title": "Divvy API",
+    "url": "http://divvybikes.com/stations/json",
+    "description": "List of stations and available bikes for Chicago's bike share system."
+  },
+  {
+    "title": "Open 311",
+    "url": "http://dev.cityofchicago.org/docs/api",
+    "description": "Open311 is a form of technology that provides open channels of communication for issues that concern public space and public services. Primarily, Open311 refers to a standardized protocol for location-based collaborative issue-tracking.",
+    "links": [
+      {"name": "The Launch of Open311 in Chicago", "url": "http://open311.org/2012/09/the-launch-of-open311-in-chicago/"},
+      {"name": "Service Tracker", "url": "http://servicetracker.cityofchicago.org/"}
+    ]
+  },
+  {
+    "title": "ClearPath REST API",
+    "url": "http://api1.chicagopolice.org/clearpath/documentation",
+    "description": "Chicago Police Department data on community concerns, crime, mugshots, most wanted and events.",
+    "links": [
+      {"name": "Smart Chicago blog post", "url": "http://www.smartchicagocollaborative.org/using-the-new-clearpath-api-to-help-communities-interact-with-the-chicago-police-department/"}
+    ]
+  },
+  {
+    "title": "Cook County Data Portal",
+    "url": "http://data.cookcountyil.gov/",
+    "description": "Open data portal for Cook County government.",
+    "links": [
+      {"name": "Tutorials", "url": "http://blog.cookcountygov.com/opencc/tutorials-how-to-use-open-data/"}
     ]
   },
 ]
