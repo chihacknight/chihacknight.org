ff042df7ec724a5cd0f201e832e817cf6bc0d0ec
Derek Eder <derek.eder@gmail.com>
2013-11-12 19:47:58 -0600

    added resources page and stubbed out a few data endpoints

---
+++ data/hack_night_data.js
@@ -0,0 +1,20 @@
+var hack_night_data = [
+  {
+    "title": "Chicago Data Portal",
+    "url": "http://data.cityofchicago.org",
+    "description": "Official data portal for the City of Chicago. Contains hundreds of datasets including  crime, health, energy, budgets, salaries and more!",
+    "links": [
+      {"name": "Soda 2.0 API", "url": "http://dev.socrata.com/consumers/getting-started"},
+      {"name": "Energy Usage 2010 API", "url": "https://data.cityofchicago.org/developers/docs/energy-usage-2010"},
+    ]
+  },
+  {
+    "title": "Chicago Transit Authority API",
+    "url": "http://www.transitchicago.com/developers/default.aspx",
+    "description": "API for the CTA buses and trains.",
+    "links": [
+      {"name": "CTA Train Tracker API", "url": "http://www.transitchicago.com/developers/traintracker.aspx"},
+      {"name": "Bus Tracker API", "url": "http://www.transitchicago.com/developers/bustracker.aspx"},
+    ]
+  },
+]
