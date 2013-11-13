db4b1739bffd371ce381e86c64ac943c66be868b
Juan-Pablo Velez <jpvelez@gmail.com>
2013-11-12 21:24:25 -0600

    added census.ire, ihs data portal, state of il, and made tweaks to data page.

--- data/hack_night_data.js
+++ data/hack_night_data.js
@@ -4,17 +4,17 @@ var hack_night_data = [
     "url": "http://data.cityofchicago.org",
     "description": "Official data portal for the City of Chicago. Contains hundreds of datasets including  crime, health, energy, budgets, salaries and more!",
     "links": [
-      {"name": "Soda 2.0 API", "url": "http://dev.socrata.com/consumers/getting-started"},
-      {"name": "Energy Usage 2010 API", "url": "https://data.cityofchicago.org/developers/docs/energy-usage-2010"}
+      {"name": "Docs: Socrata API", "url": "http://dev.socrata.com/consumers/getting-started"},
+      {"name": "Docs: Energy Usage 2010 API", "url": "https://data.cityofchicago.org/developers/docs/energy-usage-2010"}
     ]
   },
   {
-    "title": "CTA API",
+    "title": "Chicago Transit Authority API",
     "url": "http://www.transitchicago.com/developers/default.aspx",
     "description": "API for the Chicago Transit Authority (CTA) buses and trains.",
     "links": [
-      {"name": "CTA Train Tracker API", "url": "http://www.transitchicago.com/developers/traintracker.aspx"},
-      {"name": "Bus Tracker API", "url": "http://www.transitchicago.com/developers/bustracker.aspx"}
+      {"name": "Docs: CTA Train Tracker API", "url": "http://www.transitchicago.com/developers/traintracker.aspx"},
+      {"name": "Docs: Bus Tracker API", "url": "http://www.transitchicago.com/developers/bustracker.aspx"}
     ]
   },
   {
@@ -23,20 +23,20 @@ var hack_night_data = [
     "description": "List of stations and available bikes for Chicago's bike share system."
   },
   {
-    "title": "Open 311",
+    "title": "Open 311 API",
     "url": "http://dev.cityofchicago.org/docs/api",
     "description": "Open311 is a form of technology that provides open channels of communication for issues that concern public space and public services. Primarily, Open311 refers to a standardized protocol for location-based collaborative issue-tracking.",
     "links": [
-      {"name": "The Launch of Open311 in Chicago", "url": "http://open311.org/2012/09/the-launch-of-open311-in-chicago/"},
-      {"name": "Service Tracker", "url": "http://servicetracker.cityofchicago.org/"}
+      {"name": "News: The Launch of Open311 in Chicago", "url": "http://open311.org/2012/09/the-launch-of-open311-in-chicago/"},
+      {"name": "App: Service Tracker", "url": "http://servicetracker.cityofchicago.org/"}
     ]
   },
   {
-    "title": "ClearPath REST API",
+    "title": "Chicago Police ClearPath API",
     "url": "http://api1.chicagopolice.org/clearpath/documentation",
     "description": "Chicago Police Department data on community concerns, crime, mugshots, most wanted and events.",
     "links": [
-      {"name": "Smart Chicago blog post", "url": "http://www.smartchicagocollaborative.org/using-the-new-clearpath-api-to-help-communities-interact-with-the-chicago-police-department/"}
+      {"name": "News: Smart Chicago blog post on ClearPath API", "url": "http://www.smartchicagocollaborative.org/using-the-new-clearpath-api-to-help-communities-interact-with-the-chicago-police-department/"}
     ]
   },
   {
@@ -44,7 +44,29 @@ var hack_night_data = [
     "url": "http://data.cookcountyil.gov/",
     "description": "Open data portal for Cook County government.",
     "links": [
-      {"name": "Tutorials", "url": "http://blog.cookcountygov.com/opencc/tutorials-how-to-use-open-data/"}
+      {"name": "Tutorials: How to Use Open Data", "url": "http://blog.cookcountygov.com/opencc/tutorials-how-to-use-open-data/"}
+    ]
+  },
+  {
+    "title": "State of Illinois Data Portal",
+    "url": "https://data.illinois.gov/",
+    "description": "Open data portal for the State of Illinois.",
+  },
+  {
+    "title": "census.ire.org",
+    "url": "http://census.ire.org/",
+    "description": "Easiest way to get demographic data from the Census Bureau - way easier than census.gov.",
+    "links": [
+      {"name": "News: Knight funding expands IRE’s journalist-friendly Census site", "url": "http://www.niemanlab.org/2012/10/knight-funding-expands-ires-journalist-friendly-census-site/"}
+    ]
+  },
+  {
+    "title": "Institute for Housing Studies Data Portal",
+    "url": "http://www.housingstudies.org/dataportal/info/",
+    "description": "Data on housing trends for Chicago-area communities.",
+    "links": [
+      {"name": "Tutorial: Exploring Property Sales Data", "url": "http://www.housingstudies.org/news/blog/exploring-property-sales-data-ihs-data-portal/"},
+      {"name": "Tutorial: Using Mortgage Data", "url": "http://www.housingstudies.org/news/blog/using-mortgage-data-ihs-data-portal/"},
     ]
   }
 ]
