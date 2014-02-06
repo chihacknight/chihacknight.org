81da8160e0f5e3bc0d8361d1384dd629c9788e23
Derek Eder <derek.eder@gmail.com>
2014-02-06 15:04:50 -0600

    added link to google group; closed #40, swapped out data potluck for intro to programming, added open city credit

--- data/hack_night_events.js
+++ data/hack_night_events.js
@@ -2,7 +2,7 @@ var hack_night_events = [
     {
         "title": "Get Covered Illinois",
         "date": "2014-02-04",
-        "description": "<p><a href='https://twitter.com/nzorach'>Nat Zorach</a> from <a href='http://handbuiltcity.org/'>The Handbuilt City</a> will discuss how they use <a href='https://fundrise.com/'>Fundrise</a> to spur the development of affordable housing in Gary, IN and St Louis, MO..</p>",
+        "description": "<p><a href='https://twitter.com/nzorach'>Nat Zorach</a> from <a href='http://handbuiltcity.org/'>The Handbuilt City</a> will discuss how they use <a href='https://fundrise.com/'>Fundrise</a> to spur the development of affordable housing in Gary, IN and St Louis, MO.</p>",
         "sponsor": "<a href='http://datamade.us/'>DataMade</a>",
         "links": [
             {
