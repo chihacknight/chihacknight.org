048b4c900d48bf79c4b5f5d1b9b7f4ada7f5af6f
Derek Eder <derek.eder@gmail.com>
2014-04-14 12:05:10 -0500

    text change for aca outreach app

--- data/hack_night_events.js
+++ data/hack_night_events.js
@@ -2,7 +2,7 @@ var hack_night_events = [
     {
         "title": "Affordable Care Act Outreach App",
         "date": "2014-04-15",
-        "description": "<p><a href='https://www.linkedin.com/in/dominiquerwilliams'>Dominique Williams</a>, <a href='https://twitter.com/shua123'>Josh Kalov</a> and <a href='https://twitter.com/civicwhitaker'>Christopher Whitaker</a> will present on the <a href='http://www.smartchicagocollaborative.org/projects/civic-innovation-in-chicago/affordable-care-act-outreach-app/'>Affordable Care Act Outreach App</a> they built in partnership with the <a href='http://www.smartchicagocollaborative.org/'>Smart Chicago Collaborative</a> and <a href='http://www.lisc-chicago.org/index.html'>LISC-Chicago</a>.</p><p>Through their efforts, LISC helped to enroll 2,975 Chicagoans for health insurance under the Affordable Care Act.</p>",
+        "description": "<p><a href='https://www.linkedin.com/in/dominiquerwilliams'>Dominique Williams</a> and <a href='https://twitter.com/shua123'>Josh Kalov</a> will present on the <a href='http://www.smartchicagocollaborative.org/projects/civic-innovation-in-chicago/affordable-care-act-outreach-app/'>Affordable Care Act Outreach App</a> built by <a href='http://www.smartchicagocollaborative.org/'>Smart Chicago Collaborative</a> for <a href='http://www.lisc-chicago.org/index.html'>LISC-Chicago</a> as part of the <a href='http://www.smartchicagocollaborative.org/projects/civic-innovation-in-chicago/'>Civic Works Project</a>.</p><p>Through their efforts, LISC helped to enroll 2,975 Chicagoans for health insurance under the Affordable Care Act.</p>",
         "sponsor": "<a href='http://www.socrata.com'>Socrata</a>",
         "links": [
             {
